4b73d6341e67c950281ee06293bcba04ad3eff41
timbradyy <124389347+timbradyy@users.noreply.github.com>
2023-06-11 22:25:12 +0000

    donley

--- js/script.js
+++ js/script.js
@@ -1,5 +1,22 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2020 Mr.Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Tim Brady
+// Created on: May 2023
 // This file contains the JS functions for index.html
+
+"use strict"
+
+function myButtonClicked() {
+  const userAge = document.getElementById("age").value
+  document.getElementById("age").innerHTML = userAge
+  const day = document.getElementById("day").value
+  document.getElementById("day").innerHTML = day
+
+  if (day == "tuesday" || day == "thursday" || (userAge > "12" && userAge < "21")) {
+    document.getElementById("price").innerHTML =
+      "You are eligible for discounted pricing."
+  } else {
+    document.getElementById("price").innerHTML =
+      "You are not eligible for discounted pricing."
+  }
+}
